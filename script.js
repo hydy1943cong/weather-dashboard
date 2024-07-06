af23e1bd5ff5f065b503f79fdada694126cae46e
Cong Wang <wangcong525@hotmail.com>
2024-07-06 12:23:48 -0400

    forcast done

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const APIkey="3b41a3a2d317ea4220e8e87edf0ba6f5";
-const requestUrl = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=3b41a3a2d317ea4220e8e87edf0ba6f5&units=imperial";
+const currentUrl = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=3b41a3a2d317ea4220e8e87edf0ba6f5&units=imperial";
+const futureUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=3b41a3a2d317ea4220e8e87edf0ba6f5&units=imperial";
 
-fetch (requestUrl)
+fetch (currentUrl)
     .then (function (response){
         return response.json();
     })
@@ -26,4 +27,27 @@ fetch (requestUrl)
           }
         )
     
-   
+fetch (futureUrl)
+.then (function (response){
+    return response.json();
+})
+.then (function(forcast){  
+  console.log(forcast);
+  for (i=7; i<forcast.list.length; i=i+8){
+            const Day1El = document.createElement('div');
+            const Day1DateEl = document.createElement('h4');
+            const Day1IconEl = document.createElement('img');
+            const Day1TempEl = document.createElement("p");
+            const Day1WindEl = document.createElement("p");
+            const Day1Humidity=document.createElement("p");
+            Day1DateEl.textContent=dayjs.unix(forcast.list[i].dt).format("MM/DD/YYYY");
+            Day1IconEl.src=`https://openweathermap.org/img/w/${forcast.list[i].weather[0].icon}.png`;
+            Day1TempEl.textContent= `Temp: ${forcast.list[i].main.temp} °F`;
+            Day1WindEl.textContent=`Wind: ${forcast.list[i].wind.speed} MPH`;
+            Day1Humidity.textContent=`Humidity: ${forcast.list[i].main.humidity} %`;
+            Day1DateEl.append(Day1IconEl);
+            Day1El.append(Day1DateEl,Day1TempEl,Day1WindEl,Day1Humidity);
+            document.body.appendChild(Day1El);
+  }
+}
+    )  
